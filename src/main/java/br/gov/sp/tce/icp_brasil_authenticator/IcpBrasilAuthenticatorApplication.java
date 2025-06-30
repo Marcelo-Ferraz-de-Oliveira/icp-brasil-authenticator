f931e7f2dca36e928f64d6bcc9c2d76cf9bf8694
Marcelo Ferraz de Oliveira <mfoliveira@tce.sp.gov.br>
2025-06-30 13:48:52 -0300

    Feat: commit inicial

---
+++ src/main/java/br/gov/sp/tce/icp_brasil_authenticator/IcpBrasilAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.tce.icp_brasil_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcpBrasilAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcpBrasilAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/tce/icp_brasil_authenticator/IcpBrasilAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.tce.icp_brasil_authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcpBrasilAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
